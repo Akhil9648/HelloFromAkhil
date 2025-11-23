04a8160838b5c8ad6a5129abe2319db2195944a6
Akhil <akhilpandey494@gmail.com>
2025-11-23 17:27:55 +0530

    Remove unnecessary comment about tailwindcss in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// You do NOT need to import tailwindcss as a Vite plugin!
 export default defineConfig({
+  base: '/',           
   plugins: [react()],
 })
